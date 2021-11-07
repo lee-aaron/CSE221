3af861d8bae25374393f199202771d5e990f5275
Benjamin Yang <ben@Benjamins-MacBook-Pro.local>
2021-11-07 13:01:52 -0800

    Working page fault code

--- memory/src/main.c
+++ memory/src/main.c
@@ -13,10 +13,10 @@ int main(int argc, char **argv)
   read_end();
   double cycle_per_s = (double)(tend - tstart) / 10.0;
 
-  int num = 1000;
+  int num = 100;
   access_time(num);
-  //read_bandwidth(500, cycle_per_s);
-  //write_bandwidth(100, cycle_per_s);
+  read_bandwidth(100, cycle_per_s);
+  write_bandwidth(100, cycle_per_s);
   printf("Start running Page Fault Time\n");
   fault_time(num);
 }
--- memory/src/page_fault.c
+++ memory/src/page_fault.c
@@ -18,22 +18,35 @@ void fault_time(int num)
 
   int fd = open(filename, O_RDONLY);
   int pagesize = getpagesize();
+  struct stat stats;
+  fstat(fd, &stats);
+  int filesize = stats.st_size;
   int size = pagesize*num;
 
   printf("%d byte pages\n", pagesize);
+  printf("file %s @ %d bytes\n", filename, filesize);
+
+  if(size > filesize)
+  {
+    int pages = ceil((filesize)/(double)pagesize);
+    size = pages*pagesize;
+  }
   printf("Calling mmap on %d bytes\n", size);
 
-  void * mem = mmap(0, size, PROT_NONE, MAP_PRIVATE, fd, 0);
-  
-  char c;
+  void * mem = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fd, 0);
+  printf("Memory mapped");
 
   read_start();
+  printf("Timer started");
+  char c;
   for(int i=0; i<size; i+=pagesize)
   {
     c = *((char *)mem+i);
   }
   read_end();
+  printf("Timer ended");
   double cycles = (double)(tend - tstart)/(double)num;
+  printf("Number of cycles: %f\n", cycles);
 
   write_fault_time(filename, cycles);
 
