32ee5a96bed9cffbe90b97a071a01f374f6286a2
lee-aaron <lee.aaron.68@gmail.com>
2021-11-09 08:18:19 -0800

    Removed debugging code and added exit statement

--- memory/src/page_fault.c
+++ memory/src/page_fault.c
@@ -10,21 +10,6 @@
 #include "page_fault.h"
 #include "utils.h"
 
-typedef long long int num;
-
-FILE *fp;
-
-void readone(num *x) { fscanf(fp, "%lld ", x); }
-void readunsigned(unsigned long long *x) { fscanf(fp, "%llu ", x); }
-void readstr(char *x) { fscanf(fp, "%s ", x); }
-void readchar(char *x) { fscanf(fp, "%c ", x); }
-
-void printone(char *name, num x) { printf("%20s: %lld\n", name, x); }
-void printonex(char *name, num x) { printf("%20s: %016llx\n", name, x); }
-void printunsigned(char *name, unsigned long long x) { printf("%20s: %llu\n", name, x); }
-void printchar(char *name, char x) { printf("%20s: %c\n", name, x); }
-void printstr(char *name, char *x) { printf("%20s: %s\n", name, x); }
-
 void fault_time(int num, double cps)
 {
   char filename[] = "page_fault_time.txt";
@@ -44,9 +29,11 @@ void fault_time(int num, double cps)
 #else
     system("sync; echo 3 > /proc/sys/vm/drop_caches");
 #endif
-    void *mem = mmap(NULL, pagesize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, offset);
-    if (mem == MAP_FAILED)
+    void *mem = mmap(NULL, pagesize, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, offset);
+    if (mem == MAP_FAILED) {
       perror("mmap");
+      exit(1);
+    }
     offset += pagesize;
     read_start();
     for (int j = 0; j < pagesize; j++)
@@ -58,62 +45,6 @@ void fault_time(int num, double cps)
     times[i] = cycles / (cps / 1e9);
     munmap(mem, pagesize);
   }
-
-  int pid = getpid();
-  char buff[1024];
-  sprintf(buff, "/proc/%d/stat", pid);
-
-  fp = fopen(buff, "r");
-  if (fp == NULL)
-  {
-    perror("fopen");
-    exit(1);
-  }
-
-  long long int pidt;
-  char tcomm[PATH_MAX];
-  char state;
-
-  long long int ppid;
-  long long int pgid;
-  long long int sid;
-  long long int tty_nr;
-  long long int tty_pgrp;
-
-  long long int flags;
-  long long int min_flt;
-  long long int cmin_flt;
-  long long int maj_flt;
-  long long int cmaj_flt;
-
-  readone(&pidt);
-  readstr(tcomm);
-  readchar(&state);
-  readone(&ppid);
-  readone(&pgid);
-  readone(&sid);
-  readone(&tty_nr);
-  readone(&tty_pgrp);
-  readone(&flags);
-  readone(&min_flt);
-  readone(&cmin_flt);
-  readone(&maj_flt);
-  readone(&cmaj_flt);
-
-  printone("pid", pid);
-  printstr("tcomm", tcomm);
-  printchar("state", state);
-  printone("ppid", ppid);
-  printone("pgid", pgid);
-  printone("sid", sid);
-  printone("tty_nr", tty_nr);
-  printone("tty_pgrp", tty_pgrp);
-  printone("flags", flags);
-  printone("min_flt", min_flt);
-  printone("cmin_flt", cmin_flt);
-  printone("maj_flt", maj_flt);
-  printone("cmaj_flt", cmaj_flt);
-  fclose(fp);
 
   write_fault_time(filename, times, num);
 
